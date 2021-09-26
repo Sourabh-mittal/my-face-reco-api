843193f9fa913352356b31a39262c438ea320f45
Sourabh-mittal <myfavasta@gmail.com>
2021-09-26 07:53:36 +0530

    my first Project

---
+++ controllers/image.js
@@ -0,0 +1,32 @@
+const Clarifai = require("Clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "717e273eb2c44015b966a3f6457a7948",
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json("some error"));
+};
+const handleImage = (req, res, db) => {
+  const { id } = req.body;
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0]);
+    })
+    .catch((err) => {
+      res.status(400).json("unable to get entries");
+    });
+};
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall,
+};
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
+
+const signin = require("./controllers/signin");
+const register = require("./controllers/register");
+const profile = require("./controllers/profile");
+const image = require("./controllers/image");
+
+const db = knex({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    user: "every",
+    password: "EVERY",
+    database: "my-face-reco",
+  },
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json("success");
+});
+
+app.post("/signin", signin.handleSignin(db, bcrypt));
+
+app.post("/register", (req, res) => {
+  register.handleRegister(req, res, db, bcrypt);
+});
+
+app.get("/profile/:id", (req, res) => {
+  profile.handleProfile(req, res, db);
+});
+
+app.put("/image", (req, res) => {
+  image.handleImage(req, res, db);
+});
+
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
+app.listen(3000, () => {
+  console.log("app is runnig");
+});
