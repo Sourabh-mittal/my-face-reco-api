89ae993657b1877ff04356a2fa10450ee761a105
Sourabh-mittal <myfavasta@gmail.com>
2021-09-28 13:16:06 +0530

    Adding the files

--- server.js
+++ server.js
@@ -46,5 +46,5 @@ app.post("/imageurl", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App is running on port ${procees.env.PORT}`);
+  console.log(`App is running on port `);
 });
